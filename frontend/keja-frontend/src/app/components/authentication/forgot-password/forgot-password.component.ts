b85b73f94de9e93e95247cd1acdc60ce425f9094
JAPHETHNYARANGA <nyaranga4@gmail.com>
2025-03-22 12:10:40 +0300

    💄 created login register and forgot password pages

--- frontend/keja-frontend/src/app/components/authentication/forgot-password/forgot-password.component.ts
+++ frontend/keja-frontend/src/app/components/authentication/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent {
+  constructor(private router:Router){}
 
+  navigateToLogin(){
+    this.router.navigate(['login']);
+  }
 }
--- frontend/keja-frontend/src/app/components/authentication/login/login.component.ts
+++ frontend/keja-frontend/src/app/components/authentication/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import AOS from 'aos';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  constructor(private router:Router){}
+
+  ngOnInit(){
+    AOS.init();
+  }
+
+  isPasswordVisible = false; 
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  navigateToRegister(){
+    this.router.navigate(['register']);
+  }
+
+  navigateToForgotPass(){
+    this.router.navigate(['forgot-pass']);
+  }
 
 }
